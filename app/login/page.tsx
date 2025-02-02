416d3ed6c7c5accd24539b5b6546ee846fbe425f
Arsenic-01 <vedbhor25@gmail.com>
2025-02-02 16:35:37 +0530

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,12 @@
 import LoginPage from "@/components/LoginPage";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Suspense } from "react";
 const page = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-24 px-5">
-      <LoginPage />
+      <Suspense fallback={<Skeleton className="w-full h-[400px]" />}>
+        <LoginPage />
+      </Suspense>
     </main>
   );
 };
