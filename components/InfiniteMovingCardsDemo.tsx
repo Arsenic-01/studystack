a567d131e6fc64ad9ef979936b037e1a2139397c
Arsenic-01 <vedbhor25@gmail.com>
2025-02-17 20:15:48 +0530

    update infinitemovingcards

--- components/InfiniteMovingCardsDemo.tsx
+++ components/InfiniteMovingCardsDemo.tsx
@@ -1,16 +1,25 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
 
 export function InfiniteMovingCardsDemo() {
   return (
-    <div className="md: rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-      <InfiniteMovingCards
-        items={testimonials}
-        direction="right"
-        speed="slow"
-      />
+    <div className="py-32 2xl:py-44 rounded-md flex flex-col antialiased bg-white gap-10 dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+      <div className="flex flex-col gap-5  text-center">
+        {" "}
+        <h1 className="text-5xl 2xl:text-6xl px-5 font-bold bg-gradient-to-r dark:text-transparent  dark:from-white dark:to-neutral-400 light:from-black light:to-gray-700 bg-clip-text white:text-transparent">
+          Testimonials
+        </h1>
+        <span className="text-lg text-neutral-600 dark:text-neutral-400">
+          Supported by the best
+        </span>
+      </div>
+      <div className="w-full max-w-4xl px-5">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+      </div>
     </div>
   );
 }
@@ -27,27 +36,27 @@ const testimonials = [
     //Bhosale maam
     quote:
       "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-    name: "William Shakespeare",
+    name: "Bhosale maam",
     title: "Hamlet",
   },
   {
     //Ushir maam
     quote: "All that we see or seem is but a dream within a dream.",
-    name: "Edgar Allan Poe",
+    name: "Ushir maam",
     title: "A Dream Within a Dream",
   },
   {
     //Mahajan sir
     quote:
       "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-    name: "Jane Austen",
+    name: "Mahajan sir",
     title: "Pride and Prejudice",
   },
   {
     //Vidhate mam
     quote:
       "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-    name: "Herman Melville",
+    name: "Vidhate mam",
     title: "Moby-Dick",
   },
 ];
