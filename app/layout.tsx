5a4ac296dba735ad47900b632fb9d591345caf88
Vedant <vedbhor25@gmail.com>
2025-05-05 14:26:24 +0530

    Added Vercel Speed Insights
    Connected VSI to repo - upgraded to vercel pro plan

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "sonner";
 import "./globals.css";
 import { Providers } from "./providers";
 import ReactQueryProvider from "./QueryProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({
           <ReactQueryProvider>
             <Header />
             {children}
+            <SpeedInsights />
             <Toaster richColors />
             <Footer />
           </ReactQueryProvider>
