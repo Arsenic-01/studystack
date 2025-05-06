3aa7df5ad92a370207dd999a5773809db9fcc0ac
Vedant Bhor <157885594+Arsenic-01@users.noreply.github.com>
2025-05-06 16:43:15 +0530

    Update Fuzzy.tsx

--- components/misc/Fuzzy.tsx
+++ components/misc/Fuzzy.tsx
@@ -50,7 +50,7 @@ export const ExampleContent = () => {
       <div className="flex items-center justify-center gap-3">
         <Link
           target="_blank"
-          href="https://docs.google.com/forms/d/e/1FAIpQLSeCgACy0cfy08L_CGsxputmIIqvh-aD4uUE7B-sX1oIzqwZ9g/viewform?usp=sharing"
+          href="/contact"
           className="w-fit px-4 py-2 font-semibold text-neutral-700 bg-neutral-200 transition-colors hover:bg-neutral-300 dark:text-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700"
         >
           Contact
