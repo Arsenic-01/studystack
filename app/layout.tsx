d778226683fdb86ad301ac1170102741e2ce81ca
Vedant Bhor <157885594+Arsenic-01@users.noreply.github.com>
2026-03-01 16:57:13 +0530

    Refactor script tag for cleaner layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,10 +103,7 @@ export default function RootLayout({
           content="BbWKkb9nYRja3XHwtN7JOnQmxh4AQo1Rea5eWW1Zueo"
         />
         <script src="https://cdn.botpress.cloud/webchat/v3.6/inject.js"></script>
-        <script
-          src="https://files.bpcontent.cloud/2026/02/20/21/20260220211729-5R65VZTP.js"
-          defer
-        ></script>
+        <script src="https://files.bpcontent.cloud/2026/02/20/21/20260220211729-5R65VZTP.js" defer></script>
       </head>
       <body className={`${dmSans.className} antialiased`}>
         <Providers>
